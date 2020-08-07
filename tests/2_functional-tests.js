7d77dc1db708fc956c87ac0b281139783004c1ec
Glitch (spiral-bevel-debt) <none>
2020-08-07 10:33:28 +0000

    🌑🍶 Checkpoint ./tests/2_functional-tests.js:19066909/202

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -52,10 +52,7 @@ suite('Functional Tests', function() {
         .query({input: '3/7.2//4kg'})
         .end(function(err, res){
           assert.equal(res.status, 200);
-          assert.equal(res.body.initNum, 10);
-          assert.equal(res.body.initUnit, 'L');
-          assert.approximately(res.body.returnNum, 2.64172, 0.1);
-          assert.equal(res.body.returnUnit, 'gal');
+          assert.equal(res.body, {"error":"invalid number"});
           done();
       });  
       
